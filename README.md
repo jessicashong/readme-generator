dacc5c8ae5627122ab110f6bbc935ef08e63ceb3
jessicashong <jessicashong12@gmail.com>
2022-12-12 18:32:50 -0600

    udpated readme skeleton

---
+++ README.md
@@ -0,0 +1,31 @@
+# OnlyFlix
+
+## Description
+    OnlyFlix is a web application designed to help a user select a top movie from a collection of  genres.
+    
+    ## Table of contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [How to Contribute](#how-to-contribute)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    - [License](#license)
+    
+    ## Installation
+    
+    
+    ## Usage
+    Navigate to https://rosalvamartinez.github.io/OnlyFlix/ and select a genre or scroll down to find the genre of your choice. Use the navigation arrows to scroll through the top ten movies of each genre.
+    
+    ## How to Contribute
+    
+    
+    ## Tests
+    
+    
+    ## Questions 
+    If you have futher questions or would like to give any feedback, please email me at example@gmail.com.
+    
+    ## License
+    MIT license    
+    
--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-// TODO: Include packages needed for this application
+//packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-//readme frame--replace with user input
-const generateREADME = ({ title, description, installation, usage, contributions, tests, license, contact }) => 
-    `# ${title}
+//readme skeleton--replace with user input
+const generateREADME = ({ title, description, installation, usage, contributions, tests, license, github, contact }) => 
+`# ${title}
 
-    ## Description
-    ${description}
-    
-    ## Table of contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [How to Contribute](#how-to-contribute)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    - [License](#license)
-    
-    ## Installation
-    ${installation}
-    
-    ## Usage
-    ${usage}
-    
-    ## How to Contribute
-    ${contributions}
-    
-    ## Tests
-    ${tests}
-    
-    ## Questions 
-    If you have futher questions or would like to give any feedback, please email me at ${contact}.
-    
-    ## License
-    ${license}    
-    `;
+## Description
+${description}
+
+## Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## How to Contribute
+${contributions}
+
+## Tests
+${tests}
+
+## Questions 
+If you have futher questions or would like to give any feedback, please feel free to contact me through email.
+[GitHub: ](http://www.github.com/${github});
+[Email: ](${contact})
+
+## License
+${license}    
+`;
 
 // array of questions for user input
 inquirer
@@ -71,16 +73,26 @@ inquirer
     },
     {
         message: 'Please choose a license.',
-        type: 'input',
+        type: 'list',
+        choices: ['MIT License','Apache License 2.0','Mozilla Public License 2.0','GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License','BSD 3-Clause "New" or "Revised" License','Boost Software License 1.0','Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0','GNU Affero General Public License v3.0','GNU General Public License v2.0','GNU Lesser General Public License v2.1','The Unlicense 2.0'],
         name: 'license'
     },
     {
-        message: 'Please provide an email address for contacting purposes.',
+        message: 'Please provide your GitHub username for any further questions.',
+        type: 'input',
+        name: 'github'
+    },
+    {
+        message: 'Please provide an email address for any further questions.',
         type: 'input',
         name: 'contact'
     }
 ])
+//conditional: if nothing was entered for section, omit from readme
 .then((data) => {
+if (data === '') {
+
+}
 const readmeContent = generateREADME(data);
 
 fs.writeFile('README.md', readmeContent, (err) => 
@@ -91,3 +103,6 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
